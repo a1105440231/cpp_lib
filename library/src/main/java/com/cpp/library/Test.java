860dcbf57c302c3612d1de41651c0877893d7ed1
ChenPP <ChenPP>
2018-11-03 16:44:10 +0800

    init

---
+++ library/src/main/java/com/cpp/library/Test.java
@@ -0,0 +1,15 @@
+package com.cpp.library;
+
+/**
+ * Created by CPP on 2018-11-03
+ */
+public class Test {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    
+}
